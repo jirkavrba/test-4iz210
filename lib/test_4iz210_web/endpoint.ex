45a5adc66c7495e0e665af7c8721b47f34c6327a
Jiří Vrba <jiri@vrba.dev>
2022-10-28 09:11:50 +0200

    Move the favicon under static files

--- lib/test_4iz210_web/endpoint.ex
+++ lib/test_4iz210_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule Test4iz210Web.Endpoint do
     at: "/",
     from: :test_4iz210,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images favicon.png robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
