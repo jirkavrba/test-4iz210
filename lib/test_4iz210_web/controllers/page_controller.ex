3755d6d5bc7b332edce846715064f1896fb44a4e
Jiří Vrba <jiri@vrba.dev>
2022-10-28 09:11:20 +0200

    Remove the original Phoenix HTML

--- lib/test_4iz210_web/controllers/page_controller.ex
+++ lib/test_4iz210_web/controllers/page_controller.ex
@@ -1,7 +1,17 @@
 defmodule Test4iz210Web.PageController do
   use Test4iz210Web, :controller
 
+  import Test4iz210.AssociativeRules, only: [generate_associative_rule_question: 0]
+  import Test4iz210.ConfusionMatrix, only: [generate_confusion_matrix_question: 0]
+  import Test4iz210.Entropy, only: [generate_entropy_question: 0]
+  import Test4iz210.EuclideanDistance, only: [generate_euclidean_distance_question: 0]
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> assign(:associative_rules_question, generate_associative_rule_question())
+    |> assign(:confusion_matrix_question, generate_confusion_matrix_question())
+    |> assign(:entropy_question, generate_entropy_question())
+    |> assign(:euclidean_distance_question, generate_euclidean_distance_question())
+    |> render("index.html")
   end
 end
